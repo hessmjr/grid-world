32d9cd929108b2cce265bdb5dc2a65269c50ec06
Mark <mark.hess2@gmail.com>
2019-10-20 19:03:57 -0500

    setup config file

--- src/main/java/com/hessmjr/mdp/Agent.java
+++ src/main/java/com/hessmjr/mdp/Agent.java
@@ -1,6 +1,5 @@
 package com.hessmjr.mdp;
 
-import burlap.behavior.singleagent.learning.LearningAgent;
 import burlap.behavior.singleagent.learning.tdmethods.QLearning;
 import burlap.behavior.singleagent.planning.Planner;
 import burlap.behavior.singleagent.planning.stochastic.policyiteration.PolicyIteration;
@@ -9,38 +8,35 @@ import burlap.mdp.singleagent.SADomain;
 import burlap.statehashing.simple.SimpleHashableStateFactory;
 
 
-class AgentFactory {
+class Agent {
 
-    private static final String VALUE_ITERATION = "Value Iteration";
-    private static final String POLICY_ITERATION = "Policy Iteration";
-    private static final String Q_LEARNER = "Q-Learning";
+    static final String VALUE_ITERATION = "Value Iteration";
+    static final String POLICY_ITERATION = "Policy Iteration";
+    static final String Q_LEARNER = "Q-Learning";
 
     // constants that all planning algorithms will utilize
-    private static final double GAMMA = 0.99;
     private static final SimpleHashableStateFactory HASH_FACTORY = new SimpleHashableStateFactory();
 
     /**
      * Main planner building method which decides which planner is being requested then calls
      * the appropriate helper method
-     * @param name - String of planner to build
      * @param domain - SADomain the planner will process
      * @return Planner
      */
-    static Planner buildAgent(String name, SADomain domain, int maxIters) {
-        double maxDelta = 0.0001;
+    static Planner buildAgent(SADomain domain, int maxIters) {
 
-        switch (name) {
+        switch (Config.AGENT_NAME) {
             case VALUE_ITERATION:
-                return buildValueIteration(domain, maxIters, maxDelta);
+                return buildValueIteration(domain, maxIters);
 
             case POLICY_ITERATION:
-                return buildPolicyIteration(domain, maxIters, maxDelta);
+                return buildPolicyIteration(domain, maxIters);
 
             case Q_LEARNER:
                 return buildQLearner(domain, maxIters);
 
             default:
-                throw new IllegalArgumentException("Invalid planner requested: " + name);
+                throw new IllegalArgumentException("Invalid planner requested: " + Config.AGENT_NAME);
         }
     }
 
@@ -48,22 +44,20 @@ class AgentFactory {
      * Builds a value iteration planner
      * @param domain - SADomain to process
      * @param maxIters - int of max iterations
-     * @param maxDelta - double of max delta shared by all planners
      * @return ValueIteration Planner
      */
-    private static Planner buildValueIteration(SADomain domain, int maxIters, double maxDelta) {
-        return new ValueIteration(domain, GAMMA, HASH_FACTORY, maxDelta, maxIters);
+    private static Planner buildValueIteration(SADomain domain, int maxIters) {
+        return new ValueIteration(domain, Config.GAMMA, HASH_FACTORY, Config.MAX_DELTA, maxIters);
     }
 
     /**
      * Builds a policy iteration planner
      * @param domain - SADomain to process
      * @param maxIters - int of max iterations
-     * @param maxDelta - double of max delta shared by all planners
      * @return PolicyIteration Planner
      */
-    private static Planner buildPolicyIteration(SADomain domain, int maxIters, double maxDelta) {
-        return new PolicyIteration(domain, GAMMA, HASH_FACTORY, maxDelta, maxIters, maxIters);
+    private static Planner buildPolicyIteration(SADomain domain, int maxIters) {
+        return new PolicyIteration(domain, Config.GAMMA, HASH_FACTORY, Config.MAX_DELTA, maxIters, maxIters);
     }
 
     /**
@@ -72,25 +66,15 @@ class AgentFactory {
      * @param maxIters - int of max iterations
      * @return QLearning Planner
      */
-    private static QLearning buildQLearner(SADomain domain, int maxIters) {
+    static QLearning buildQLearner(SADomain domain, int maxIters) {
 
         // hyper params that can be tweaked with the learning agent
         double initQ = 0.5;
         double alpha = 0.3;
 
         // build the new Q learning agent and setup for planning experiments
-        QLearning agent = new QLearning(domain, GAMMA, HASH_FACTORY, initQ, alpha, maxIters);
+        QLearning agent = new QLearning(domain, Config.GAMMA, HASH_FACTORY, initQ, alpha, maxIters);
         agent.initializeForPlanning(1);
         return agent;
-    }
-
-    /**
-     * Calls Q learning builder for learning experiment usage
-     * @param domain - SADomain
-     * @param maxIters - int of max iterations
-     * @return LearningAgent
-     */
-    static LearningAgent buildAgent(SADomain domain, int maxIters) {
-        return buildQLearner(domain, maxIters);
     }
 }
---
+++ src/main/java/com/hessmjr/mdp/Config.java
@@ -0,0 +1,28 @@
+package com.hessmjr.mdp;
+
+
+class Config {
+
+    /*
+    Contains all configurations utilized within this project.  Any changes necessary to which
+    agent to train, which domain to build, the particular parameters for both of those items
+    as well as any other specifics will be stored in this class as constants
+     */
+
+    // Main settings
+    static final int NUM_TRIALS = 1_000;
+    static final String DOMAIN_NAME = Domain.GRID_WORLD_SMALL;
+    static final String AGENT_NAME = Agent.Q_LEARNER;
+    static final boolean VISUALIZE = false;
+    static final boolean PLOT = true;
+    static final boolean Q_LEARN_PLOT = false;
+
+    // Agent settings
+    static final Double GAMMA = 0.99;
+    static final double MAX_DELTA = 0.0001;
+
+    // Domain settings
+    static final double SUCCESS_RATE = 0.8;
+    static final double GOAL_REWARD = 5.0;
+    static final double MOVE_REWARD = -0.1;
+}
